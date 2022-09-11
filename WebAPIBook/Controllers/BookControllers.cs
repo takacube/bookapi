91fc0377a82a2fd71764f29748d23e713eb2205f
tashim <tashim@rinna.co.jp>
2022-09-11 15:03:32 +0900

    :sparkles: create new endpoint

---
+++ WebAPIBook/Controllers/BookControllers.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIBook.Controllers
+{
+    [ApiController]
+    [Route("books")]
+    public class BookControllers : ControllerBase
+    {
+        [HttpGet]
+        [Route("list")]
+        public string Get()
+        {
+            return "sample";
+        }
+        [HttpPost]
+        [Route("subscription")]
+        public string subscribe(string title, string author, int evaluation)
+        {
+            string _title = title;
+
+            string _author = author;
+
+            int _evaluation = evaluation;
+
+            string res = string.Format("This is a {0} by {1}. my evaluation is {2}", _title, _author, _evaluation);
+            return res;
+        }
+    }
+}
